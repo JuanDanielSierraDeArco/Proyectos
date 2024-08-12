1f91be4a44070a718f6c280644404fff0c3a7994
Juan_Sierra <juandanieljdsda10@gmail.com>
2024-08-12 16:39:04 -0500

    pandas

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import element
 import read_csv
 import charts
+import pandas as pd
 
 def run():
+  
   data = read_csv.read_csv('Data_pop.csv')
   contin_list = list(set(map( lambda x: x['Continent'],data)))
   print('!Puedes elegir un continente!\n')
@@ -10,9 +12,18 @@ def run():
    print('=>',a)
   save_cont = input('\n=>')
   save_cont = save_cont.title()
+
+  '''
   data = list(filter(lambda item: item['Continent'] == save_cont, data))
   country = list(map(lambda x: x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'],data))
+  '''
+
+  df = pd.read_csv('Data_pop.csv')
+  df = df[df['Continent'] == save_cont]
+  country = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+  
 
   country_list = list(set(country.copy()))
   charts.generate_tort_char(save_cont, country, percentages)
