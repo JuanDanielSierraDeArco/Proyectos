6c9eed9425e1aad73befb1e1681047fb2996c6ce
Juan_Sierra <juandanieljdsda10@gmail.com>
2024-08-12 11:21:12 -0500

    add docs

---
+++ app/README.md
@@ -0,0 +1,18 @@
+# App proyect graficas de poblacion
+
+Para correr esta app debes seguir la siguientes instruciones en la terminal
+
+#ubicar el archivo 
+
+```sh
+cd juego_1_ppt
+python3 main.py
+```
+#app proyect
+``sh
+git clone
+cd app
+python3 -m venv env_app
+source env_app/bin/activate
+pip3 install -r requirements.txt
+python3 main.py
---
+++ app/charts.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+labels = ['a', 'b', 'c', 'd']
+values = [20, 50, 20, 10]
+
+def generate_bar_char(name, labels, values):
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    ax.set_title('Gráfico de Barras')
+    plt.savefig(f'./imgs/{name}.png')
+    plt.close()
+
+def generate_tort_char(name, labels, values):
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels, autopct="%0.1f %%")
+    ax.axis('equal')
+    ax.set_title('Gráfico de torta')
+    plt.savefig(f'./imgs/{name}tort.png')
+    plt.close()
+
+if __name__ == '__main__':
+    generate_bar_char(name,labels, values)
+    generate_tort_char(name,labels, values)
